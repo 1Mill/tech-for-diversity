bced7f585404c2139e67a4c2397071ff2dc84126
1Mill <erik.ekberg@outlook.com>
2018-11-03 11:19:43 -0700

    Scope api to v1 and keep default paths for everything

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users,
-  	path: '',
-  	path_names: {
-		sign_in: 'login',
-		sign_out: 'logout',
-		registration: 'signup'
-	}
+	scope 'api/v1/', defaults: {format: :json} do
+		devise_for :users,
+	    	path_names: {
+		  		sign_in: 'login',
+		  		sign_out: 'logout',
+		  		registration: 'signup'
+		  	}
+
+		devise_scope :user do
+			get 'users/current', :to => 'users/sessions#show'
+		end
+	end
 end
