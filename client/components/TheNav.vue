70128ed18b8cb2d24dad5cf1645748a0374a825a
1Mill <erik.ekberg@outlook.com>
2018-11-06 15:24:03 -0800

    Add pages + vuetify default layout

---
+++ client/components/TheNav.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+	data () {
+		return {
+			open: true,
+			links: [
+				{name: 'Home', to: '/'},
+				{name: 'About', to: '/about'},
+				{name: 'Login', to: '/login'},
+				{name: 'Sign Up', to: '/sign-up'}
+			]
+		}
+	}
+}
+</script>
+
+<template>
+	<div>
+		<v-toolbar-side-icon fixed bottom right
+		@click.native='open = !open'
+		/>
+
+		<v-bottom-nav :value='open' fixed>
+			<v-btn
+			v-for='(link, index) in links' :key='index'
+			:to='link.to'
+			@click='open = !open'
+			>
+				{{ link.name }}
+			</v-btn>
+		</v-bottom-nav>
+	</div>
+</template>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,5 +1,19 @@
+<script>
+import TheNav from '@/components/TheNav'
+
+export default {
+	components: {
+		TheNav
+	}
+}
+</script>
+
 <template>
-	<div>
-		<nuxt/>
-	</div>
+	<v-app>
+		<the-nav/>
+
+		<v-container>
+			<nuxt/>
+		</v-container>
+	</v-app>
 </template>
