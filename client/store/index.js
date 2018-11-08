7bf982cf1e07dd17d08d0e8dec16f2a5596864ab
1Mill <erik.ekberg@outlook.com>
2018-11-08 14:51:30 -0800

    Add store action to login user

--- client/store/index.js
+++ client/store/index.js
@@ -3,7 +3,22 @@ import Vuex from 'vuex'
 const createStore = () => {
 	return new Vuex.Store ({
 		state: () => ({
-		})
+		}),
+		actions: {
+			loginUser ({ commit }, { email, password }) {
+				this.$auth.loginWith('local', {
+					data: {
+						user: {
+							email: email,
+							password: password
+						}
+					}
+				})
+				.catch(e => {
+					console.log(e)
+				})
+			}
+		}
 	})
 }
 
