b29271c93be8fc201d79e16c3dd26da5df6a79db
1Mill <erik.ekberg@outlook.com>
2018-11-10 12:44:13 -0800

    Create project templates

---
+++ client/components/project/ProjectHeader.vue
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<template>
+	<div>
+	 	Project header
+	</div>
+</template>
--- client/pages/projects/_id.vue
+++ client/pages/projects/_id.vue
@@ -1,7 +1,20 @@
 <script>
 import { mapState } from 'vuex'
 
+import ProjectDescription from '@/components/project/ProjectDescription'
+import ProjectHeader from '@/components/project/ProjectHeader'
+import ProjectLinkList from '@/components/project/ProjectLinkList'
+import ProjectSkillList from '@/components/project/ProjectSkillList'
+import ProjectTechnologyList from '@/components/project/ProjectTechnologyList'
+
 export default {
+	components: {
+		ProjectDescription,
+		ProjectHeader,
+		ProjectLinkList,
+		ProjectSkillList,
+		ProjectTechnologyList
+	},
 	data () {
 		return {
 			project: {}
@@ -20,11 +33,13 @@ export default {
 </script>
 
 <template>
-	<div>
-		SINGLE PROJECT PAGE
-
-		<br /><br />
-
+	<v-container>
 		{{ project }}
-	</div>
+
+		<project-description></project-description>
+		<project-header></project-header>
+		<project-link-list></project-link-list>
+		<project-skill-list></project-skill-list>
+		<project-technology-list></project-technology-list>
+	</v-container>
 </template>
