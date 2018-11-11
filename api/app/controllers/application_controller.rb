f18366090f00bcd6da6033925440e32ac7670bc6
1Mill <erik.ekberg@outlook.com>
2018-11-11 14:05:31 -0800

    Respond with unauthorized

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::API
 	include ActionController::MimeResponds
 
 	include Pundit
+	rescue_from Pundit::NotAuthorizedError, :with => :user_not_authorized
 
 	respond_to :json
+
+	private
+
+	def user_not_authorized
+		render json: {}, status: :unauthorized
+	end
 end
