6ee55ba4cb4fb881ef10c8014ecfeddb45e6d935
1Mill <erik.ekberg@outlook.com>
2018-11-11 14:58:09 -0800

    Add form + submit method

--- client/pages/projects/_id/edit.vue
+++ client/pages/projects/_id/edit.vue
@@ -1,8 +1,19 @@
 <script>
+export default {
+	methods: {
+		submitForm () {
+			console.log('Edits submitted')
+		}
+	}
+}
 </script>
 
 <template>
-	<div>
-		EDITING THE PAGE!
-	</div>
+	<v-container>
+		<v-form @submit.prevent='submitForm'>
+			<v-btn type='submit'>
+				Submit
+			</v-btn>
+		</v-form>
+	</v-container>
 </template>
