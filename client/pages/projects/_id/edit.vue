5f685bfda7da155ee550029400150fe17d2dd200
1Mill <erik.ekberg@outlook.com>
2018-11-11 19:11:20 -0800

    Populate form with current project values

--- client/pages/projects/_id/edit.vue
+++ client/pages/projects/_id/edit.vue
@@ -1,5 +1,34 @@
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
+	data () {
+		return {
+			title: null,
+			issue_area: null,
+			location_city: null,
+			location_state: null,
+			status: null,
+			description: null
+		}
+	},
+	computed: {
+		...mapGetters ('projects', [
+			'getProjectById'
+		])
+	},
+	created () {
+		// Get project by id
+		const id = this.$route.params.id
+		const project = this.getProjectById(id)
+
+		// 1. Get project key-pair values (e.g. ["title", "Something"])
+		// 2. Break down mutli-demtnional array to single array (e.g. [["title", "Something"], ["description", "Crazy stuff"]] => ["title":"Something", "description":"CrazyStuff"])
+		// 3. Assign key-pairs to data (e.g. this.title = array['title'])
+		// This allows us to (a) initilize data and (b) make it reactive
+		_.assign(this, _.fromPairs(_.toPairsIn(project)))
+
+	},
 	methods: {
 		submitForm () {
 			console.log('Edits submitted')
