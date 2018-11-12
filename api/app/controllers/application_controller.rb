263ce10c092e7882df7286c63ff0757e004b06d6
1Mill <erik.ekberg@outlook.com>
2018-11-12 14:14:02 -0800

    Remove pundit

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::API
 	include ActionController::MimeResponds
-
-	include Pundit
-	rescue_from Pundit::NotAuthorizedError, :with => :user_not_authorized
-
 	respond_to :json
-
-	private
-
-	def user_not_authorized
-		render json: {}, status: :unauthorized
-	end
 end
