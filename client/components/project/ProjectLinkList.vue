fe3020e116f8f6d00b06e2c59c403bcfc24b2e0e
1Mill <erik.ekberg@outlook.com>
2018-11-13 15:19:26 -0800

    Add temp links to project

--- client/components/project/ProjectLinkList.vue
+++ client/components/project/ProjectLinkList.vue
@@ -2,7 +2,17 @@
 </script>
 
 <template>
-	<div>
-	 	Project Link List
-	</div>
+	<v-container>
+		<h1 class='[ headline ]'>
+			Links
+		</h1>
+		<v-list dense>
+			<v-list-tile
+			v-for='i in 7' :key='i'
+			to='/'
+			>
+				http://somerandomlink_{{ i }}.com
+			</v-list-tile>
+		</v-list>
+	</v-container>
 </template>
