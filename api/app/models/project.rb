88412fc2dbd59fe64969d7b978bf0dc6102c84a6
1Mill <erik.ekberg@outlook.com>
2018-11-15 15:26:28 -0800

    Set enum keys for project vars

--- api/app/models/project.rb
+++ api/app/models/project.rb
@@ -1,3 +1,27 @@
 class Project < ApplicationRecord
-  belongs_to :user
+	belongs_to :user
+
+	enum stage: {
+		open: 0,
+		in_progress: 1,
+		complete: 2
+	}
+
+	enum state: {
+		wa: 0
+	}
+
+	enum company_kind: {
+		resturant: 0,
+		tailor: 1
+	}
+
+	enum update_frequency: {
+		very_frequently: 0,
+		frequently: 1,
+		occasionally: 2,
+		rarely: 3,
+		very_rarely: 4,
+		never: 5
+	}
 end
--- api/spec/fabricators/project_fabricator.rb
+++ api/spec/fabricators/project_fabricator.rb
@@ -1,7 +1,7 @@
 Fabricator(:project) do
   user              nil
   company           { Faker::Company.name }
-  stage             0
+  stage             { Project.stages.keys.sample }
 
   name_first        { Faker::Name.first_name }
   name_last         { Faker::Name.last_name }
@@ -11,16 +11,16 @@ Fabricator(:project) do
   address_street_1  { Faker::Address.street_address }
   address_street_2  { Faker::Address.secondary_address }
   address_city      { Faker::Address.city }
-  address_state     0
+  address_state     { Project.states.keys.sample }
   address_zipcode   { Faker::Address.zip_code }
 
-  company_kind      0
+  company_kind      { Project.company_kinds.keys.sample }
   mission_statement { Faker::Hobbit.quote }
   who_you_are       { Faker::Company.bs }
   who_you_help      { Faker::Company.profession }
   what_you_do       { Faker::Company.industry }
 
   current_services  "MyText"
-  update_frequency  0
+  update_frequency  { Project.update_frequencies.keys.sample }
   domain_registered false
 end
