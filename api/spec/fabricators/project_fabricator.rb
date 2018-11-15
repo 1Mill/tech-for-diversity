6afd21ea0a527ae2c895b653d465db632ab2c7cb
1Mill <erik.ekberg@outlook.com>
2018-11-15 15:17:25 -0800

    Random project attributes

--- api/spec/fabricators/project_fabricator.rb
+++ api/spec/fabricators/project_fabricator.rb
@@ -1,26 +1,26 @@
 Fabricator(:project) do
   user              nil
-  company           "MyString"
-  stage             ""
+  company           { Faker::Company.name }
+  stage             0
 
-  name_first        "MyString"
-  name_last         "MyString"
-  email             "MyString"
-  phone             "MyString"
+  name_first        { Faker::Name.first_name }
+  name_last         { Faker::Name.last_name }
+  email             { Faker::Internet.email }
+  phone             { Faker::PhoneNumber.phone_number }
 
-  address_street_1  "MyString"
-  address_street_2  "MyString"
-  address_city      "MyString"
-  address_state     ""
-  address_zipcode   "MyString"
+  address_street_1  { Faker::Address.street_address }
+  address_street_2  { Faker::Address.secondary_address }
+  address_city      { Faker::Address.city }
+  address_state     0
+  address_zipcode   { Faker::Address.zip_code }
 
-  company_kind      ""
-  mission_statement "MyText"
-  who_you_are       "MyText"
-  who_you_help      "MyText"
-  what_you_do       "MyText"
+  company_kind      0
+  mission_statement { Faker::Hobbit.quote }
+  who_you_are       { Faker::Company.bs }
+  who_you_help      { Faker::Company.profession }
+  what_you_do       { Faker::Company.industry }
 
   current_services  "MyText"
-  update_frequency  1
+  update_frequency  0
   domain_registered false
 end
